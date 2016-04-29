b5742a8694c2582e40273e81361fb155222e9f78
leetou <leetou@fnguide.com>
2016-04-29 16:15:44 +0900

    remove files

--- getSource.js
+++
@@ -1,9 +0,0 @@
-function get_source (document_body) {
-    console.log('in page');
-    return document_body.innerText;
-}
-
-chrome.extension.sendMessage({
-    action: "getSource",
-    source: get_source(document.body)
-});
